772f2cf809dd155d7031f7fd257f47945598704c
buildingmachines <alanwilliamswork@gmail.com>
2020-04-29 12:14:28 +0000

    security group linked to instance

--- deploy_single_server/main.tf
+++ deploy_single_server/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "basic_centos_image" {
 	ami="ami-09e5afc68eed60ef4"
 	instance_type="t2.micro"
+	vpc_security_group_ids=[aws_security_group.tcp_in_from_anywhere.id]
 
 	tags={
 		Name="basic_centos_test_server"
@@ -13,7 +14,7 @@ resource "aws_instance" "basic_centos_image" {
 
 resource "aws_security_group" "tcp_in_from_anywhere" {
 	name="tcp_in_from_anywhere"
-	
+
 	ingress {
 		from_port=8080
 		to_port=8080
